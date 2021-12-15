40f2ca38a45f7ccf801f3942735768fca0b79fb7
Harry Jubb <harry@cresset-group.com>
2021-12-15 09:29:22 +0000

    (TW-4046) Remove @returns

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ import { TXDAConnection, TXDAMessageHandlers } from "./types"
  * Request a connection to the Torx Design-Analyze application that the current page is embedded in.
  * @param url The URL of the Torx Design-Analyze installation that the current page is embedded in. Used to ensure that only messages from the correct origin URL are accepted.
  * @param handlers Event handlers to execute when events are received from Torx Design-Analyze.
- * @returns
  */
 const initialize = (url: string, handlers: TXDAMessageHandlers = {}): Promise<TXDAConnection> =>
   new Promise((resolve, reject) => {
